fae63353607654cfb8e5354b75968b31280cd28c
AcKerMani <49419946+AcKerMani@users.noreply.github.com>
2020-04-23 18:19:59 +0800

    lol up-4-23 master

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,85 +1,83 @@
-
-import { details } from '../data/lol_details_duowan'
+import {details} from '../data/lol_details_duowan'
 // pages/detail/detail.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    details
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        details
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    console.log(options.id)
-    console.log(details)
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        console.log(options.id)
+        console.log(details)
 
-    const heroIntro = details.find(item => {
-      return item.id === options.id
-    })
-    console.log(heroIntro)
+        const heroIntro = details.find(item => {
+            return item.id === options.id
+        })
+        console.log(heroIntro)
 
-    this.setData({
-      details: heroIntro
-    })
+        this.setData({
+            details: heroIntro
+        })
 
-    console.log(this.data.details)
-    // 设置 标题
-    wx.setNavigationBarTitle({
-      title: `${heroIntro.title}-${heroIntro.name}`
-    });
+        console.log(this.data.details)
+        // 设置 标题
+        wx.setNavigationBarTitle({
+            title: `${heroIntro.title}-${heroIntro.name}`
+        })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    },
 
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    }
 })
